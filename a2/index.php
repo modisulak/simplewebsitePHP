9ff47a691e4ad1b10467ae701d2b196efd81c945
modithasulakshana <s3756843@student.rmit.edu>
2019-08-25 15:46:56 +1000

    Added the about us, prices

--- a2/index.php
+++ a2/index.php
@@ -4,42 +4,120 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Assignment 2</title>
-    
+
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="style.css">
     <script src='../wireframe.js'></script>
+
+
   </head>
 
   <body>
-
     <header>
-       <div><img src='../../media/RMIT-Logo.png' alt='RMIT University logo' height=75 />Lunardo
-      </div>
+      <center>
+        <div><img src='../../media/logo.png' alt='Company Logo' height=75  />
+        </div>
+        <div>
+          <h1>LUNADO</h1>
+        </div>
+      </center>
     </header>
 
-    <nav>
+    <nav role="full-horizontal">
       <div>
-          <ul>
-            <li><a href="default.asp">Home</a></li>
-            <li><a href="news.asp">News</a></li>
-            <li><a href="contact.asp">Contact</a></li>
-            <li><a href="about.asp">About</a></li>
-         </ul>
+        <ul class ="nav">
+          <li><a href="#ABOUT US">ABOUT US</a></li>
+          <li><a href="#PRICES">PRICES</a></li>
+          <li><a href="#NOW SHOWING">NOW SHOWING</a></li>
+          <li><a href="#SYPNOSIS">SYPNOSIS</a></li>
+        </ul>
       </div>
     </nav>
 
     <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/cinema.jpg' alt='Cinema' height=1100/>
+
+
+      <article id='ABOUT US'>
+        <div class="container">
+          <div class="centered">ABOUT US</div>
+        </div>
+        <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
+        <div class="parallax">
+
+
+        </div>
       </article>
+
+      <!-- One -->
+      <section id="one" class="wrapper style1">
+        <div class="inner">
+          <article class="feature left">
+            <span class="image"><img src='../../media/logo.png' alt="" /></span>
+            <div class="content">
+              <h2>Integer vitae libero acrisus egestas placerat  sollicitudin</h2>
+              <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
+
+            </div>
+          </article>
+          <article class="feature right">
+            <span class="image"><img src='../../media/logo.png' alt="" /></span>
+            <div class="content">
+              <h2>Integer vitae libero acrisus egestas placerat  sollicitudin</h2>
+              <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
+
+            </div>
+          </article>
+          <article class="feature left">
+            <span class="image"><img src='../../media/logo.png' alt="" /></span>
+            <div class="content">
+              <h2>Integer vitae libero acrisus egestas placerat  sollicitudin</h2>
+              <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
+
+            </div>
+          </article>
+        </div>
+      </section>
+
+      <!-- Three -->
+      <section id="three" class="wrapper style3 special">
+        <div class="inner">
+          <header class="major narrow	">
+            <h2>Magna sed consequat tempus</h2>
+            <p>Ipsum dolor tempus commodo turpis adipiscing Tempor placerat sed amet accumsan</p>
+          </header>
+          <ul class="actions">
+            <li><a href="#" class="button big alt">Magna feugiat</a></li>
+          </ul>
+        </div>
+      </section>
+      
+<div>
+ <div>
+     <div class="label">LABEL</div>
+     <div>filler</div>
+ </div>
+  <div>
+     <div class="label">LABEL</div>
+     <div>filler</div>
+ </div>
+  <div>
+     <div class="label">LABEL</div>
+     <div>filler</div>
+ </div>
+</div>
+
+      <article id='SYPNOSIS'> 
+        <h2 'TEST'> </h2>
+        <img src='../../media/cinema.jpg' alt='Cinema' height=600/>
+      </article>
+
     </main>
 
     <footer>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+        </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
