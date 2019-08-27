d3349cc41ea93dc9731909f72530a2d97479a2f5
modithasulakshana <s3756843@student.rmit.edu>
2019-08-27 23:33:32 +1000

    Added the prices and the pictures

--- a2/index.php
+++ a2/index.php
@@ -16,10 +16,10 @@
   <body>
     <header>
       <center>
-        <div><img src='../../media/logo.png' alt='Company Logo' height=75  />
+        <div><img src='../../media/logo.png' alt='Company Logo' style="margin:5px 0px" height=55  />
         </div>
         <div>
-          <h1>LUNADO</h1>
+          <h2>LUNADO</h2>
         </div>
       </center>
       <nav role="full-horizontal">
@@ -54,7 +54,7 @@
       <section id="one" class="wrapper style1">
         <div class="inner">
           <article class="feature left">
-            <span class="image"><img src='../../media/logo.png' alt="" /></span>
+            <span class="image"><img src='https://images.pexels.com/photos/109669/pexels-photo-109669.jpeg?cs=srgb&dl=architecture-auditorium-chairs-109669.jpg&fm=jpg' alt="" /></span>
             <div class="content">
               <h2>About Us</h2>
               <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
@@ -88,93 +88,160 @@
           </article>
         </div>
       </section>
+      <a class = "anchor" id= "PRICES"> </a>
+      <h1><center>PRICES</center></h1>
 
       <!-- Three -->
       <section id="PRICES1" class="section3">
         <a class = "anchor" id= "PRICES"> </a>
-        <h1><center>PRICES</center></h1>
+
         <section id="prices" class="flex-container" style = margin:0>
           <div class="pricesbox">
 
-
-          </div>
-          <div class="pricesbox">
-            <img src='../../media/website-under-construction.png' alt="Avengers Poster" class="movieCardImage">
-
-
-          </div>
-          <div class="pricesbox">
-            <img src='../../media/website-under-construction.png' alt="Avengers Poster" class="movieCardImage">
-
-
-          </div>
-        </section>
-
-      </section>
-
-      <section>
-        <a class = "anchor" id= "NOW SHOWING"> </a>
-        <h1><center>NOW SHOWING</center></h1>
-        
-        <article class="flex-container">
-
-          <div class="movieCard">
-            <img src='../../media/website-under-construction.png' alt="Avengers Poster" class="movieCardImage">
-            <h1 class="movieCardTitle">Avengers Endgame (M) </h1>
-            <p class="movieCardText"> <br> Wednesday- 9:00PM<br><br> Thursday- 9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p>
-          </div>
-
-          <div class="movieCard">
-            <img src='../../media/website-under-construction.png' alt="Top End Wedding Poster" class="movieCardImage">
-            <h1 class="movieCardTitle">Top End Wedding (M) </h1>
-            <p class="movieCardText"> <br> Wednesday-9:00PM<br><br> Thursday-9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p> 
-          </div>
-
-          <div class="movieCard">
-            <img src='../../media/website-under-construction.png' alt="Dumbo Poster" class="movieCardImage">
-            <h1 class="movieCardTitle">Dumbo (M) </h1>
-            <p class="movieCardText"> <br> Wednesday-9:00PM<br><br> Thursday-9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p> 
-          </div>
-
-          <div class="movieCard">
-            <img src='../../media/website-under-construction.png' alt="Happy Prince Poster" class="movieCardImage">
-            <h1 class="movieCardTitle">The Happy Prince (M) </h1>
-            <p class="movieCardText"> <br> Wednesday-9:00PM<br><br> Thursday-9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p> 
-          </div>
-        </article>
-      </section>
-
-      <section style>
-        <div class=synopsis-box>
-          <div class="flex-container">
-            <span> 
-              <h1> Avengers Endgame (M) </h1> 
-              <p> THis is the synopsis of this movie. THis is the synopsis of this movie.THis is the synopsis of this movie.THis is the synopsis of this movie.THis is the synopsis of this movie. </p>
-            </span>
-            <img src='../../media/website-under-construction.png' alt="avengers" class="synopsis-image" >       </div>
-          <div class="synopsis-bar"> 
             <div class="flex-container"> 
-              <p> MAKE A BOOKING: <br> 
-                <button> WED 9:00pm</button>
-                <button> THU 9:00pm</button>
-                <button> FRI 9:00pm</button>
-                <button> SAT 6:00pm</button>
-                <button> SUN 6:00pm</button>
-              </p> 
+              <p> ADULT PRICES <br> 
+              </p>
+
+              <table>
+                <tr>
+                  <th>Seat</th>
+                  <th>Monday,Wednesday AND 12pm on Weekdays</th>
+                  <th>All other Times</th>
+                </tr>
+                <tr>
+                  <td>Standard Adult</td>
+                  <td>$14.00</td>
+                  <td>$19.80</td>
+                </tr>
+                <tr>
+                  <td>First Class Adult</td>
+                  <td>$24.00</td>
+                  <td>$30.00</td>
+                </tr>
+              </table>
+
             </div>
           </div>
+
+          <div class="pricesbox">
+            <div class="flex-container"> 
+              <p> CHILD PRICES <br> 
+              <table>
+                <tr>
+                  <th>Seat</th>
+                  <th>Monday,Wednesday AND 12pm on Weekdays</th>
+                  <th>All other Times</th>
+                </tr>
+                <tr>
+                  <td>Standard Adult</td>
+                  <td>$14.00</td>
+                  <td>$19.80</td>
+                </tr>
+                <tr>
+                  <td>First Class Adult</td>
+                  <td>$24.00</td>
+                  <td>$30.00</td>
+                </tr>
+              </table>
+              </p>
+          </div>
+
+
+          </div>
+        <div class="pricesbox">
+          <div class="flex-container"> 
+            <p> CONCESSION PRICES <br> 
+            <table>
+              <tr>
+                <th>Seat</th>
+                <th>Monday,Wednesday AND 12pm on Weekdays</th>
+                <th>All other Times</th>
+              </tr>
+              <tr>
+                <td>Standard Adult</td>
+                <td>$14.00</td>
+                <td>$19.80</td>
+              </tr>
+              <tr>
+                <td>First Class Adult</td>
+                <td>$24.00</td>
+                <td>$30.00</td>
+              </tr>
+            </table>
+            </p>
+        </div>
+
+
         </div>
       </section>
 
-    </main>
+    </section>
 
-    <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-        </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
+  <section>
+    <a class = "anchor" id= "NOW SHOWING"> </a>
+    <h1><center>NOW SHOWING</center></h1>
 
-  </body>
+    <article class="flex-container">
+
+      <div class="movieCard">
+        <img src='https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg' alt="Avengers Poster" class="movieCardImage">
+        <h1 class="movieCardTitle">Avengers Endgame (M) </h1>
+        <p class="movieCardText"> <br> Wednesday- 9:00PM<br><br> Thursday- 9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p>
+      </div>
+
+      <div class="movieCard">
+        <img src='https://m.media-amazon.com/images/M/MV5BMjU0NDk0N2EtNTliZS00MjNmLTk0M2MtYTMzOTUxMGQwZWI3XkEyXkFqcGdeQXVyMzE0MTQ2NzQ@._V1_SY999_CR0,0,738,999_AL_.jpg' alt="Top End Wedding Poster" class="movieCardImage">
+        <h1 class="movieCardTitle">Top End Wedding (M) </h1>
+        <p class="movieCardText"> <br> Wednesday-9:00PM<br><br> Thursday-9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p> 
+      </div>
+
+      <div class="movieCard">
+        <img src='https://m.media-amazon.com/images/M/MV5BNjMxMDE0MDI1Ml5BMl5BanBnXkFtZTgwMzExNTU3NjM@._V1_SY1000_CR0,0,675,1000_AL_.jpg' alt="Dumbo Poster" class="movieCardImage">
+        <h1 class="movieCardTitle">Dumbo (M) </h1>
+        <p class="movieCardText"> <br> Wednesday-9:00PM<br><br> Thursday-9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p> 
+      </div>
+
+      <div class="movieCard">
+        <img src='https://m.media-amazon.com/images/M/MV5BODVjZThlMzMtZjQwNy00YjRlLWE5ZTMtMWVlMWUwM2U1NjRkXkEyXkFqcGdeQXVyODcyODY1Mzg@._V1_SY1000_SX675_AL_.jpg' alt="Happy Prince Poster" class="movieCardImage">
+        <h1 class="movieCardTitle">The Happy Prince (M) </h1>
+        <p class="movieCardText"> <br> Wednesday-9:00PM<br><br> Thursday-9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p> 
+      </div>
+    </article>
+  </section>
+
+  <section> 
+    <a class = "anchor" id= "SYPNOSIS"> </a>
+    <h1><center>SYPNOSIS</center></h1>
+    <div class=synopsis-box>
+      <div class="flex-container">
+        <span> 
+          <h1> Avengers Endgame (M) </h1> 
+          <p> THis is the synopsis of this movie. THis is the synopsis of this movie.THis is the synopsis of this movie.THis is the synopsis of this movie.THis is the synopsis of this movie. </p>
+        </span>
+        <img src='../../media/website-under-construction.png' alt="avengers" class="synopsis-image" >       </div>
+      <div class="synopsis-bar"> 
+        <div class="flex-container"> 
+          <p> MAKE A BOOKING: <br> 
+            <button> WED 9:00pm</button>
+            <button> THU 9:00pm</button>
+            <button> FRI 9:00pm</button>
+            <button> SAT 6:00pm</button>
+            <button> SUN 6:00pm</button>
+          </p> 
+        </div>
+      </div>
+    </div>
+  </section>
+
+  </main>
+
+<footer>
+  <div>&copy;<script>
+    document.write(new Date().getFullYear());
+    </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+  <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+  <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+</footer>
+
+</body>
 </html>
