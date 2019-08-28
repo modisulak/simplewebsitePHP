131030211cfbe6846dbb1a026a52a07e3543e961
s3769993 <s3769993@student.rmit.edu.au>
2019-08-28 10:21:00 +1000

    wrote simple content, cleaned up styles for synopsis and movieCard

--- a2/index.php
+++ a2/index.php
@@ -19,7 +19,7 @@
         <div><img src='../../media/logo.png' alt='Company Logo' style="margin:5px 0px" height=55  />
         </div>
         <div>
-          <h2>LUNADO</h2>
+          <h2>LUNARDO</h2>
         </div>
       </center>
       <nav role="full-horizontal">
@@ -54,35 +54,46 @@
       <section id="one" class="wrapper style1">
         <div class="inner">
           <article class="feature left">
-            <span class="image"><img src='https://images.pexels.com/photos/109669/pexels-photo-109669.jpeg?cs=srgb&dl=architecture-auditorium-chairs-109669.jpg&fm=jpg' alt="" /></span>
+            <span class="image"><img src='https://images.pexels.com/photos/109669/pexels-photo-109669.jpeg?cs=srgb&dl=architecture-auditorium-chairs-109669.jpg&fm=jpg' alt="Lux Theatre Interior" /></span>
             <div class="content">
               <h2>About Us</h2>
-              <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
+              <p>
+                  Welcome to the new and improved Lunardo Cinema! We know you've missed us, but following extensive renovations we are proud to be re-opening soon, bringing world class films and comforts to your back-door. Our roots and history are the most important things to us, and we can't wait to open up our doors and show off our brand new screens, sound and seats to all of you- familiar or new. Stay tuned and join us for the next step in our story... see you at the movies! 
+            </p>
 
             </div>
           </article>
           <article class="feature right">
-            <span class="image"><img src='../../media/atmos.jpg' alt="" /></span>
+            <span class="image"><img src='../../media/atmos.jpg' alt="Dolby Atmos Sound System Diagram" /></span>
             <div class="content">
               <h2>Dolby Atmos Sound System</h2>
-              <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
+              <p>
+                  Say hello to the next big thing in Screen and Sound... Dolby Atmos. 
+                  
+                  'Surround' doesnt do this justice, with up to 128 individual speakers placed throughout the theatre, making for the most immersive watching experience on the market. Flowing from around, above and behind, you'll feel like you're right there in the battle with your favourite Hero, or listening to every drop of rain or chatter in a quiet, thoughtful cafe. We've also upgraded to <b>3D Projectors</b> for selected shows, so stay tuned to grab your tickets quickly! 
+                </p>
 
             </div>
           </article>
           <article class="feature left">
-            <span class="image"><img src='../../media/seat.jpg' alt="" /></span>
+            <span class="image"><img src='../../media/seat.jpg' alt="Image of New Standard Chair" /></span>
             <div class="content">
-              <h2>New Chairs</h2>
-              <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
+              <h2>Upgraded Seats</h2>
+              <p>
+                  Your eyes and ears won't be the only things thanking you for choosing Lunardo.. We've also installed brand new seats for our standard theatres! These seats are so soft, the only hard part will be standing up to leave them.
+                </p>
 
             </div>
           </article>
 
           <article class="feature right">
-            <span class="image"><img src='../../media/seat2.png' alt="" /></span>
+            <span class="image"><img src='../../media/seat2.png' alt="First Class Seating Image" /></span>
             <div class="content">
-              <h2>The Premuim chairs, to be written</h2>
-              <p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
+              <h2>First Class</h2>
+              <p>
+                  Here at Lunardo we believe that every cinema experience is special... But we also understand that sometimes you feel <b>Extra Special</b>. As of our Grand re-opening, we have the just the thing- First Class. Complete with padded, reclining premium seats and in-film service, First Class is the perfect treat for yourself, or anyone special in your life. Whether you've got something to celebrate, or just need a treat, we hope you'll give it a shot.. Be warned though, it's hard to go back! Find pricing information below. 
+                
+                </p>
 
             </div>
           </article>
@@ -183,28 +194,52 @@
 
     <article class="flex-container">
 
-      <div class="movieCard">
-        <img src='https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg' alt="Avengers Poster" class="movieCardImage">
-        <h1 class="movieCardTitle">Avengers Endgame (M) </h1>
-        <p class="movieCardText"> <br> Wednesday- 9:00PM<br><br> Thursday- 9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p>
+      <div class="movieCard",id="ACT">
+        <img src='https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg' alt="Avengers Poster">
+        <h1>Avengers Endgame (M) </h1>
+        <p>
+            <br> Wednesday- 9:00PM<br>
+            <br> Thursday- 9:00PM<br>
+            <br> Friday- 9:00PM<br>
+            <br> Saturday-6:00PM<br>
+            <br> Sunday-6:00PM<br>
+        </p>
       </div>
 
-      <div class="movieCard">
-        <img src='https://m.media-amazon.com/images/M/MV5BMjU0NDk0N2EtNTliZS00MjNmLTk0M2MtYTMzOTUxMGQwZWI3XkEyXkFqcGdeQXVyMzE0MTQ2NzQ@._V1_SY999_CR0,0,738,999_AL_.jpg' alt="Top End Wedding Poster" class="movieCardImage">
-        <h1 class="movieCardTitle">Top End Wedding (M) </h1>
-        <p class="movieCardText"> <br> Wednesday-9:00PM<br><br> Thursday-9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p> 
+      <div class="movieCard",id="RMC">
+        <img src='https://m.media-amazon.com/images/M/MV5BMjU0NDk0N2EtNTliZS00MjNmLTk0M2MtYTMzOTUxMGQwZWI3XkEyXkFqcGdeQXVyMzE0MTQ2NzQ@._V1_SY999_CR0,0,738,999_AL_.jpg' alt="Top End Wedding Poster">
+        <h1>Top End Wedding (M) </h1>
+        <p> 
+            <br> Wednesday-9:00PM<br>
+            <br> Thursday-9:00PM<br>
+            <br> Friday- 9:00PM<br>
+            <br> Saturday-6:00PM<br>
+            <br> Sunday-6:00PM<br>
+        </p> 
       </div>
 
-      <div class="movieCard">
-        <img src='https://m.media-amazon.com/images/M/MV5BNjMxMDE0MDI1Ml5BMl5BanBnXkFtZTgwMzExNTU3NjM@._V1_SY1000_CR0,0,675,1000_AL_.jpg' alt="Dumbo Poster" class="movieCardImage">
-        <h1 class="movieCardTitle">Dumbo (M) </h1>
-        <p class="movieCardText"> <br> Wednesday-9:00PM<br><br> Thursday-9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p> 
+      <div class="movieCard", id="ANM">
+        <img src='https://m.media-amazon.com/images/M/MV5BNjMxMDE0MDI1Ml5BMl5BanBnXkFtZTgwMzExNTU3NjM@._V1_SY1000_CR0,0,675,1000_AL_.jpg' alt="Dumbo Poster">
+        <h1>Dumbo (M) </h1>
+        <p> 
+            <br> Wednesday-9:00PM<br>
+            <br> Thursday-9:00PM<br>
+            <br> Friday- 9:00PM<br>
+            <br> Saturday-6:00PM<br>
+            <br> Sunday-6:00PM<br>
+          </p> 
       </div>
 
-      <div class="movieCard">
-        <img src='https://m.media-amazon.com/images/M/MV5BODVjZThlMzMtZjQwNy00YjRlLWE5ZTMtMWVlMWUwM2U1NjRkXkEyXkFqcGdeQXVyODcyODY1Mzg@._V1_SY1000_SX675_AL_.jpg' alt="Happy Prince Poster" class="movieCardImage">
-        <h1 class="movieCardTitle">The Happy Prince (M) </h1>
-        <p class="movieCardText"> <br> Wednesday-9:00PM<br><br> Thursday-9:00PM<br><br> Friday- 9:00PM<br><br> Saturday-6:00PM<br><br> Sunday-6:00PM<br></p> 
+      <div class="movieCard", id="AHF">
+        <img src='https://m.media-amazon.com/images/M/MV5BODVjZThlMzMtZjQwNy00YjRlLWE5ZTMtMWVlMWUwM2U1NjRkXkEyXkFqcGdeQXVyODcyODY1Mzg@._V1_SY1000_SX675_AL_.jpg' alt="Happy Prince Poster">
+        <h1>The Happy Prince (M) </h1>
+        <p> 
+            <br> Wednesday-9:00PM<br>
+            <br> Thursday-9:00PM<br>
+            <br> Friday- 9:00PM<br>
+            <br> Saturday-6:00PM<br>
+            <br> Sunday-6:00PM<br>
+        </p> 
       </div>
     </article>
   </section>
@@ -218,7 +253,7 @@
           <h1> Avengers Endgame (M) </h1> 
           <p> THis is the synopsis of this movie. THis is the synopsis of this movie.THis is the synopsis of this movie.THis is the synopsis of this movie.THis is the synopsis of this movie. </p>
         </span>
-        <img src='../../media/website-under-construction.png' alt="avengers" class="synopsis-image" >       </div>
+        <img src='https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg' alt="avengers">       </div>
       <div class="synopsis-bar"> 
         <div class="flex-container"> 
           <p> MAKE A BOOKING: <br> 
