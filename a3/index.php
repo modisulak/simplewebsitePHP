f62b32c6dcebbd1bbf4c183f5ec7712047fc37f2
s3769993 <s3769993@student.rmit.edu.au>
2019-09-11 15:55:23 +1000

    set-up a3, implement change synopsis function

---
+++ a3/index.php
@@ -0,0 +1,411 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width initial-scale=1">
+    <title>Assignment 3</title>
+
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css">
+    <script src='../wireframe.js'></script>
+    <script scr= "script.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+
+
+  </head>
+
+  <body>
+    <header>
+      <center>
+        <div><img src='../../media/logo.png' alt="Company Logo" style="margin:5px 0px" height=55  />
+        </div>
+        <div>
+          <h2>LUNARDO</h2>
+        </div>
+      </center>
+
+
+    </header>
+    <nav role="full-horizontal">
+      <div>
+        <ul class ="nav">
+          <li><a href="#ABOUT US">ABOUT US</a></li>
+          <li><a href="#PRICES">PRICES</a></li>
+          <li><a href="#NOW SHOWING">NOW SHOWING</a></li>
+          <li><a href="#SYPNOSIS">SYNOPSIS</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <main>
+
+
+      <article id="WELCOME">
+        <div class="container">
+          <div class="centered"></div> 
+        </div>
+        <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
+        <div class="parallax">
+          <h1>Fish Creek's Finest Cinema Experience</h1>
+
+        </div>
+      </article>
+
+      <!-- One -->
+      <a class = "anchor" id= "ABOUT US"> </a>
+      <section id="one" class="wrapper style1">
+        <div class="inner">
+          <article class="feature left">
+            <span class="image"><img src="https://images.pexels.com/photos/109669/pexels-photo-109669.jpeg?cs=srgb&dl=architecture-auditorium-chairs-109669.jpg&fm=jpg" alt="Lux Theatre Interior" />
+            </span>
+            <div class="content">
+              <h2>About Us</h2>
+              <p>
+                Welcome to the new and improved Lunardo Cinema! We know you've missed us, but following extensive renovations we are proud to be re-opening soon, bringing world class films and comforts to your back-door. Our roots and history are the most important things to us, and we can't wait to open up our doors and show off our brand new screens, sound and seats to all of you- familiar or new. Stay tuned and join us for the next step in our story... see you at the movies! 
+              </p>
+
+            </div>
+          </article>
+          <article class="feature right">
+            <span class="image"><img src="../../media/atmos.jpg" alt="Dolby Atmos Sound System Diagram" /></span>
+            <div class="content">
+              <h2>Dolby Atmos Sound System</h2>
+              <p>
+                Say hello to the next big thing in Screen and Sound... Dolby Atmos. 
+
+                'Surround' doesnt do this justice, with up to 128 individual speakers placed throughout the theatre, making for the most immersive watching experience on the market. Flowing from around, above and behind, you'll feel like you're right there in the battle with your favourite Hero, or listening to every drop of rain or chatter in a quiet, thoughtful cafe. We've also upgraded to <b>3D Projectors</b> for selected shows, so stay tuned to grab your tickets quickly! 
+              </p>
+
+            </div>
+          </article>
+          <article class="feature left">
+            <span class="image"><img src='../../media/seat.jpg' alt="Image of New Standard Chair" /></span>
+            <div class="content">
+              <h2>Upgraded Seats</h2>
+              <p>
+                Your eyes and ears won't be the only things thanking you for choosing Lunardo.. We've also installed brand new seats for our standard theatres! These seats are so soft, the only hard part will be standing up to leave them.
+              </p>
+
+            </div>
+          </article>
+
+          <article class="feature right">
+            <span class="image"><img src='../../media/seat2.png' alt="First Class Seating Image" /></span>
+            <div class="content">
+              <h2>First Class</h2>
+              <p>
+                Here at Lunardo we believe that every cinema experience is special... But we also understand that sometimes you feel <b>Extra Special</b>. As of our Grand re-opening, we have the just the thing- First Class. Complete with padded, reclining premium seats and in-film service, First Class is the perfect treat for yourself, or anyone special in your life. Whether you've got something to celebrate, or just need a treat, we hope you'll give it a shot.. Be warned though, it's hard to go back! Find pricing information below. 
+
+              </p>
+
+            </div>
+          </article>
+        </div>
+      </section>
+      <a class = "anchor" id= "PRICES"> </a>
+      <h1>PRICES</h1>
+      <h3> Discount Prices during all day Mon and Tue and 12PM on Weekends</h3>
+
+      <!-- Three -->
+      <section id="pricesSection" class="section3">
+        <a class = "anchor" id= "PRICES"> </a>
+
+        <section id="prices" class="flex-container" style = margin:0>
+
+
+
+          <div class="pricesbox">
+            <div class="flex-container"> 
+
+
+              <h3> ADULT PRICES 
+              </h3>
+
+              <table> <tr class="firstRow">
+                <th>Seat</th>
+                <th>Discount Prices</th>
+                <th>Full Price</th>
+                </tr>
+
+                <tr>
+                  <td>Standard </td>
+                  <td>$14.00</td>
+                  <td>$19.80</td>
+                </tr>
+                <tr>
+                  <td>First Class</td>
+                  <td>$24.00</td>
+                  <td>$30.00</td>
+                </tr>
+              </table>
+
+
+              <h3> CHILD PRICES 
+              </h3>
+              <table> <tr class="firstRow">
+                <th>Seat</th>
+                <th>Discount Prices</th>
+                <th>Full Price</th>
+                </tr>
+
+                <tr>
+                  <td>Standard</td>
+                  <td>$11.00</td>
+                  <td>$15.30</td>
+                </tr>
+                <tr>
+                  <td>First Class</td>
+                  <td>$21.00</td>
+                  <td>$24.00</td>
+                </tr>
+              </table>
+              <div class="flex-container"> 
+                <h3>  CONCESSION PRICES </h3> 
+                <table> <tr class="firstRow">
+                  <th>Seat</th>
+                  <th>Discount Prices</th>
+                  <th>Full Price</th>
+                  </tr>
+
+                  <tr>
+                    <td>Standard</td>
+                    <td>$12.50</td>
+                    <td>$17.50</td>
+                  </tr>
+                  <tr>
+                    <td>First Class</td>
+                    <td>$22.50</td>
+                    <td>$27.00</td>
+                  </tr>
+
+                </table>
+              </div>
+
+            </div>
+          </div>
+
+
+
+        </section>
+          
+<script>
+    function clearSynopsis()
+    { 
+        alert("In the method");
+        var x = document.getElementById("synopsisACT");
+        x.style.display = "none";
+        alert("avengers cleared");
+        
+        var y = document.getElementById("synopsisRMC");
+        y.style.display = "none";
+        alert("top end cleared")
+        
+        var z = document.getElementById("synopsisANM");
+        z.style.display = "none";
+        alert("dumbo cleared");
+        
+        var a = document.getElementById("synopsisAHF");
+        a.style.display = "none";
+        alert("cleared");
+    }
+    
+    function changeSynopsis(workingId)
+    { 
+        alert("clearing current synopsis");
+        clearSynopsis();
+        alert(workingId);
+        var y = 'synopsis'+workingId;
+        alert(y);
+        var x = document.getElementById(y);
+        x.style.display = "block";   
+    }
+</script>
+          
+          
+      </section>
+      <h1>NOW SHOWING</h1>
+      <section class="nowShow4">
+        <a class = "anchor" id= "NOW SHOWING"> </a>
+
+
+        <article class="flex-container">
+          <div class="movieCard" , id="ACT" onclick= "changeSynopsis('ACT')">
+            <img src='../../media/avengers.jpg' alt="Avengers Poster">
+            <h1>Avengers Endgame (M) </h1>
+            <ul>
+              <li>Wed,Thu,Fri<br>9:00PM</li>
+              <li>Sat,Sun<br>6:00PM</li>
+            </ul>
+            
+          </div>
+
+          <div class="movieCard" , id="RMC" onclick="changeSynopsis('RMC')">
+            <img src='../../media/wedding.jpg' alt="Top End Wedding Poster">
+            <h1>Top End Wedding (M) </h1>
+            <ul> 
+              <li>Mon,Tue<br>6:00PM</li>
+              <li>Sat,Sun<br>3:00PM</li>
+            </ul> 
+          </div>
+
+          <div class="movieCard" , id="ANM" onclick="changeSynopsis('ANM')">
+            <img src='../../media/dumbo.jpg' alt="Dumbo Poster">
+            <h1>Dumbo (M)</h1>
+            <ul> 
+              <li>Mon,Tue<br>12:00PM</li>
+              <li>Wed,Thu,Fri<br>6:00PM</li>
+              <li>Sat,Sun<br>12:00PM</li>
+            </ul> 
+          </div>
+
+          <div class="movieCard" , id="AHF">
+            <img src='../../media/prince.jpg'alt="Happy Prince Poster" onclick="changeSynopsis('AHF')">
+            <h1>The Happy Prince (M) </h1>
+            <ul> 
+              <li>Wed,Thu,Fri<br>12:00PM</li>
+              <li>Sat,Sun<br>9:00PM</li>
+            </ul> 
+          </div>
+        </article>
+      </section>
+      
+      <section class="synopsis" id="synopsisACT">
+      <h1>SYNOPSIS</h1>
+      <section> 
+      <a class = "anchor" id= "SYPNOSIS"> </a>
+
+          <div class="synopsis-box">
+
+            <div class="flex-container-Image">
+              <span> 
+                <h1> Avengers Endgame (M) </h1> 
+                <p> That cheeky Thanos is at it again! Team up with all your favourite millionaires for the 23rd time in a row, as they try and fix the mess that big purple left them in following the acclaimed Infinity War (2018). Will Spiderman be back in time for the release of Spiderman 2 in a few weeks? Tune in and find out.. tickets on sale soon! </p>
+              </span>
+              <iframe src="https://www.youtube.com/embed/TcMBFSGVi1c">
+              </iframe>
+            </div>
+            <div class="synopsis-bar"> 
+              <div class="flex-containerButton"> 
+                <p> 
+                  BOOK NOW:<br> 
+                  <button class = "discount-button"> WED 9:00pm</button>
+                  <button> THU 9:00pm</button>
+                  <button> FRI 9:00pm</button>
+                  <button> SAT 6:00pm</button>
+                  <button> SUN 6:00pm</button>
+                </p> 
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+        
+        <section class="synopsis" id="synopsisRMC">
+            <h1>SYNOPSIS</h1>
+        <section> 
+          <a class = "anchor" id= "SYPNOSIS"> </a>
+
+          <div class="synopsis-box">
+
+            <div class="flex-container-Image">
+              <span> 
+                <h1> Top End Wedding (M) </h1> 
+                <p> Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis Top end wedding synopsis</p>
+              </span>
+              <iframe src="https://www.youtube.com/embed/uoDBvGF9pPU">
+              </iframe>
+            </div>
+            <div class="synopsis-bar"> 
+              <div class="flex-containerButton"> 
+                <p> 
+                  BOOK NOW:<br> 
+                  <button> MON 6:00pm</button>
+                  <button> TUE 6:00pm</button>
+                  <button > SAT 3:00pm</button>
+                  <button> SUN 3:00pm</button>
+                </p> 
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+        
+        <section class="synopsis" id="synopsisANM">
+            <h1>SYNOPSIS</h1>
+        <section> 
+          <a class = "anchor" id= "SYPNOSIS"> </a>
+
+          <div class="synopsis-box">
+
+            <div class="flex-container-Image">
+              <span> 
+                <h1> Dumbo </h1> 
+                <p> dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis dumbo synopsis  </p>
+              </span>
+              <iframe src="https://www.youtube.com/embed/7NiYVoqBt">
+              </iframe>
+            </div>
+            <div class="synopsis-bar"> 
+              <div class="flex-containerButton"> 
+                <p> 
+                  BOOK NOW:<br> 
+                  <button> MON 12:00pm</button>
+                  <button> TUE 12:00pm</button>
+                  <button > WED 6:00pm</button>
+                  <button > THU 6:00pm</button>
+                  <button > FRI 6:00pm</button>
+                  <button > SAT 12:00pm</button>
+                  <button> SUN 12:00pm</button>
+                </p> 
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+        
+        <section class="synopsis" id="synopsisAHF">
+            <h1>SYNOPSIS</h1>
+        <section> 
+          <a class = "anchor" id= "SYPNOSIS"> </a>
+
+          <div class="synopsis-box">
+
+            <div class="flex-container-Image">
+              <span> 
+                <h1> THe happy Prince (M) </h1> 
+                <p> THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis THe happy Prince synopsis  </p>
+              </span>
+              <iframe src="https://www.youtube.com/embed/tXANCJQkUIE&ab_channel=MovieclipsIndie">
+              </iframe>
+            </div>
+            <div class="synopsis-bar"> 
+              <div class="flex-containerButton"> 
+                <p> 
+                  BOOK NOW:<br> 
+                  <button> WED 12:00pm</button>
+                  <button> THU 12:00pm</button>
+                  <button > FRI 12:00pm</button>
+                  <button> SAT 9:00pm</button>
+                  <button > SUN 9:00pm</button>
+                </p> 
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+    
+        
+
+    </main>
+
+    <footer>
+      <div>&copy;<script>
+        document.write(new Date().getFullYear());
+        </script> Maximillian Young, Moditha Sulakshana, s3769993,s3756843, Max n Modi. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+    </footer>
+
+  </body>
+</html>
--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,28 @@
-/* Insert your javascript here */
+function clearSynopsis() { 
+        alert("In the method");
+        var x = document.getElementById("synopsisACT");
+        x.style.display = "none";
+        alert("avengers cleared");
+        
+        var y = document.getElementById("synopsisRMC");
+        y.style.display = "none";
+        alert("top end cleared")
+        
+        var z = document.getElementById("synopsisANM");
+        z.style.display = "none";
+        alert("dumbo cleared");
+        
+        var a = document.getElementById("synopsisAHF");
+        a.style.display = "none";
+        alert("cleared");
+    }
+    
+    function changeSynopsis(workingId) { 
+        alert("clearing current synopsis");
+        clearSynopsis();
+        alert(workingId);
+        var y = 'synopsis'+workingId;
+        alert(y);
+        var x = document.getElementById(y);
+        x.style.display = "block";   
+    }
